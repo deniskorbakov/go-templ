ac427415ecd6c08b77538395470133bdd2c27a54
Denis <korbakovd@gmail.com>
2025-12-01 17:54:36 +0300

    feat: add new config var

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,17 +2,21 @@ package config
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-	ApiPort  string `env:"API_PORT" env-default:"8080"`
-	DiagPort string `env:"DIAG_PORT" env-default:"8081"`
-	DBUrl    string `env:"DB_URL" env-default:"postgres://user:password@localhost:5432/petstore?sslmode=disable"`
+	ApiPort         string        `env:"API_PORT" env-default:"8080"`
+	DiagPort        string        `env:"DIAG_PORT" env-default:"8081"`
+	DBUrl           string        `env:"DB_URL" env-default:"postgres://user:password@localhost:5432/petstore?sslmode=disable"`
+	HTTPTimeout     time.Duration `env:"HTTP_TIMEOUT" env-default:"5s"`
+	IdleTimeout     time.Duration `env:"IDLE_TIMEOUT" env-default:"30s"`
+	ShutdownTimeout time.Duration `env:"SHUTDOWN_TIMEOUT" env-default:"30s"`
 }
 
-func MustLoad() (*Config, error) {
+func Load() (*Config, error) {
 	var cfg Config
 
 	err := cleanenv.ReadEnv(cfg)
