e5c77bdb726638ac3f85ad9c734760f70411e17e
Denis <korbakovd@gmail.com>
2025-12-04 18:51:56 +0300

    feat: create config

---
+++ internal/resource/config.go
@@ -0,0 +1,32 @@
+package resource
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	ApiPort         string        `envconfig:"API_PORT"`
+	DiagPort        string        `envconfig:"DIAG_PORT"`
+	DBUrl           string        `envconfig:"DB_URL"`
+	RedisAddress    string        `envconfig:"REDIS_ADDRESS"`
+	DbMaxConn       int           `envconfig:"DB_MAX_CONN"`
+	DbMaxIdle       int           `envconfig:"DB_MAX_IDLE"`
+	HTTPTimeout     time.Duration `envconfig:"HTTP_TIMEOUT"`
+	IdleTimeout     time.Duration `envconfig:"IDLE_TIMEOUT"`
+	ShutdownTimeout time.Duration `envconfig:"SHUTDOWN_TIMEOUT"`
+	DbMaxConnTime   time.Duration `envconfig:"DB_MAX_CONN_TIME"`
+	DbMaxIdleTime   time.Duration `envconfig:"DB_MAX_IDLE_TIME"`
+}
+
+func NewConfig() (*Config, error) {
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		return nil, fmt.Errorf("can't process the config: %w", err)
+	}
+
+	return &cfg, nil
+}
